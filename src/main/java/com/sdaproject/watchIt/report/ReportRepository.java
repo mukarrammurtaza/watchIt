fdb97f89e98fb04f584995711062d14cf82a365b
Amin A <aminqasmi78@gmail.com>
2022-11-24 22:17:52 +0500

    String to Integer
    Changed string to integer in repository files, as primary keys are now int type

--- src/main/java/com/sdaproject/watchIt/report/ReportRepository.java
+++ src/main/java/com/sdaproject/watchIt/report/ReportRepository.java
@@ -4,5 +4,5 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ReportRepository extends CrudRepository<Report, String>{
+public interface ReportRepository extends CrudRepository<Report, Integer>{
 }
