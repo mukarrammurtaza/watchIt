1db11eb91a863bb9f704a24065c740e3dd099226
Mukarram Murtaza <98780798+mukarrammurtaza@users.noreply.github.com>
2022-11-24 21:12:23 +0500

    Second commit with htmls

--- src/main/java/com/sdaproject/watchIt/report/ReportController.java
+++ src/main/java/com/sdaproject/watchIt/report/ReportController.java
@@ -1,4 +1,12 @@
 package com.sdaproject.watchIt.report;
 
+import org.springframework.web.bind.annotation.GetMapping;
+
 public class ReportController {
+    @GetMapping("")
+    public String showReportPage() {
+        return "Report";
+    }
+
 }
+
