99d22ec1b51d1844e5f1d433897cdffe9d0d8173
Mukarram Murtaza <98780798+mukarrammurtaza@users.noreply.github.com>
2022-11-25 11:11:28 +0500

    Second commit with htmls

---
+++ src/main/java/com/sdaproject/watchIt/user/MainController.java
@@ -0,0 +1,15 @@
+package com.sdaproject.watchIt.user;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+@Controller
+public class MainController {
+
+    @GetMapping("")
+    public String showHomePage() {
+        return "Home";
+    }
+
+
+
+}
