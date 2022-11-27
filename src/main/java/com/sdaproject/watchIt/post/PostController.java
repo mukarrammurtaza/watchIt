105ad8795b22d5a27656cbff377e51684209a03e
Amin A <aminqasmi78@gmail.com>
2022-11-28 00:43:53 +0500

    Search functions
    Simple and advanced search completed.
    Advanced search sorts the results from best to worst.

--- src/main/java/com/sdaproject/watchIt/post/PostController.java
+++ src/main/java/com/sdaproject/watchIt/post/PostController.java
@@ -1,4 +1,29 @@
 package com.sdaproject.watchIt.post;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import java.sql.Date;
+import java.util.List;
+
+@RestController
+@RequestMapping("/post")
 public class PostController {
+    @Autowired PostService postservices;
+    @GetMapping("/new")
+    public List<Post> getPosts() {
+        return postservices.getPosts();
+    }
+    @GetMapping("/simplesearch")
+    public List<Post> simpleSearch()
+    {
+        return postservices.simpleSearch("8 MEN");
+    }
+
+    @GetMapping("/advancedsearch")
+    public List<Post> advancedSearch()
+    {
+        return postservices.advancedSearch("8 MEN","fook","Fast", Date.valueOf("2022-11-28"));
+    }
 }
