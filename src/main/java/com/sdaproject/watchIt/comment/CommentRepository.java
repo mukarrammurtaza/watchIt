44cb20531107de2b0bc764d300ac62967694697f
faizan-codegic <108739119+faizan-codegic@users.noreply.github.com>
2022-11-29 23:04:40 +0500

    mini change

--- src/main/java/com/sdaproject/watchIt/comment/CommentRepository.java
+++ src/main/java/com/sdaproject/watchIt/comment/CommentRepository.java
@@ -1,8 +1,10 @@
 package com.sdaproject.watchIt.comment;
 
+import org.springframework.context.annotation.Primary;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+@Primary
 @Repository
 public interface CommentRepository extends CrudRepository<Comment, Integer>{
 
